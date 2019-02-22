bb5c96e91841221c7e166f31f691bcc978c18b37
Mitul Gautam <mitulgautam@outlook.com>
2019-02-22 23:29:25 +0530

    Some new Unknown Changes

---
+++ Yamaha AR-Catalog/Assets/ColorCode.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorCode : MonoBehaviour
+{
+    public GameObject[] color;
+    Color[] c=new Color[3];
+    // Start is called before the first frame update
+    void Start()
+    {
+        string text = PlayerPrefs.GetString("key");
+        string[] list = text.Split(',');
+        print(list);
+        for (int i = 0; i < int.Parse(list[5]); i++)
+        {
+            ColorUtility.TryParseHtmlString(list[6+i], out c[i]);
+            print(c[i]+"");
+        }
+        for (int i = 0; i < 3; i++)
+        { 
+             color[i].GetComponent<Image>().color = c[i];
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
