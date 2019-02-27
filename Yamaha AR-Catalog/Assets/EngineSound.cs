88385d40b2038037a66ef1ac364db98c43239668
Mitul Gautam <mitulgautam@outlook.com>
2019-02-27 23:47:17 +0530

    Audio completed

---
+++ Yamaha AR-Catalog/Assets/EngineSound.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EngineSound : MonoBehaviour
+{
+    public Sprite OffSprite;
+    public Sprite OnSprite;
+    public Button but;
+    public AudioClip[] clips;
+    public AudioSource source;
+    string text,model;
+    private void Start()
+    {
+         text = PlayerPrefs.GetString("key");
+        string[] list = text.Split(',');
+        model = list[0];
+
+    }
+    public void ChangeImage()
+    {
+        if (but.image.sprite == OnSprite)
+        {
+            but.image.sprite = OffSprite;
+
+            source.Stop();
+        }
+        else
+        {
+            but.image.sprite = OnSprite;
+
+            source.PlayOneShot(clips[int.Parse(model)]);
+        }
+        
+    }
+
+}
