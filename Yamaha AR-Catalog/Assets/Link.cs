a2f1e212487b6ded19300b338e069ebcc91f4eeb
Mitul Gautam <mitulgautam@outlook.com>
2019-02-28 00:24:14 +0530

    Video Link Completed

---
+++ Yamaha AR-Catalog/Assets/Link.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link : MonoBehaviour
+{
+    public string link;
+    public void openLink()
+    {
+        Application.OpenURL(link);
+    }
+}
